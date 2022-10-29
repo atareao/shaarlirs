81f3ac7d04f747e4d18432cd20cfebb5dbb132e9
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-10-29 10:21:42 +0200

    add test

--- src/models/link.rs
+++ src/models/link.rs
@@ -1,13 +1,13 @@
 use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Utc};
 use actix_web::web;
-use sqlx::{sqlite::{SqlitePool, SqliteRow}, Error, query, Row};
+use sqlx::{sqlite::{SqlitePool, SqliteRow, SqliteQueryResult}, Error, query, Row};
 
 use super::{metatag::Metatag, short_url, tag::Tag, link_tag::LinkTag};
 
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Eq)]
 pub struct Link {
     pub id: i64,
     pub url: String,
@@ -43,6 +43,12 @@ pub struct LinkWithTagsNew {
     updated: Option<DateTime<Utc>>,
 }
 
+impl PartialEq for Link{
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
 impl Link{
     fn from_row(row: SqliteRow) -> Link{
         Link{
@@ -57,20 +63,20 @@ impl Link{
         }
     }
 
-    pub async fn create_from_post(pool: &web::Data<SqlitePool>, link_with_tags: LinkWithTagsNew) -> Result<LinkWithTags, Error>{
-        let url = link_with_tags.url;
+    pub async fn create_from_post(pool: &web::Data<SqlitePool>, link_with_tags: &LinkWithTagsNew) -> Result<LinkWithTags, Error>{
+        let url = &link_with_tags.url;
         let metatag = Metatag::new(&url).await.unwrap();
-        let title = match link_with_tags.title {
+        let title = match &link_with_tags.title {
             Some(title) => title,
-            None => metatag.title,
+            None => &metatag.title,
         };
-        let description = match link_with_tags.description {
+        let description = match &link_with_tags.description {
             Some(description) => description,
-            None => metatag.description,
+            None => &metatag.description,
         };
-        let tags_names = match link_with_tags.tags {
+        let tags_names = match &link_with_tags.tags {
             Some(tags) => tags,
-            None => metatag.tags,
+            None => &metatag.tags,
         };
         let private = match link_with_tags.private{
             Some(private) => private,
@@ -100,8 +106,9 @@ impl Link{
         let shorturl = Self::set_shorturl(pool, link.id).await;
         for tag_name in tags_names{
             let tag = Tag::get_or_insert(&pool, &tag_name).await.unwrap();
-            let link_tag = LinkTag::create(pool, link.id, tag.id).await;
+            let _ = LinkTag::create(pool, link.id, tag.id).await;
         }
+        println!("================================");
         let tags = Tag::read_tags_for_link(&pool, link.id).await.unwrap();
         Ok(LinkWithTags {
             id: link.id,
@@ -125,31 +132,150 @@ impl Link{
             .await;
         shorturl
     }
-    pub async fn create(pool: &web::Data<SqlitePool>, url: &str) -> Result<Link, Error>{
-        
-        let sql = "INSERT INTO links (url, shorturl, title, description, private, created, updated) VALUES ($1, $2, $3, $4, $5, $6, $7);";
-        let id = query(sql)
-            .execute(pool.get_ref())
-            .await?
-        .last_insert_rowid();
-        Self::read(pool, id).await
+    pub async fn create(pool: &web::Data<SqlitePool>, url: &str) -> Result<LinkWithTags, Error>{
+        let metatag = Metatag::new(&url).await.unwrap();
+        let title = metatag.title;
+        let description = metatag.description;
+        let tags_names = metatag.tags;
+        let private = true;
+        let created = Utc::now();
+        let updated = created;
+        let link_with_tags = LinkWithTagsNew {
+            url: url.to_string(),
+            title: Some(title),
+            description: Some(description),
+            tags: Some(tags_names),
+            private: Some(private),
+            created: Some(created),
+            updated: Some(updated),
+        };
+        Self::create_from_post(pool, &link_with_tags).await
     }
 
     pub async fn read(pool: &web::Data<SqlitePool>, id: i64) -> Result<Link, Error>{
-        let sql = "SELECT id, url, shorturl, title, description, private, created, updated FROM links WHERE id = $1;";
+        let sql = "SELECT * FROM links WHERE id = $1;";
         query(sql)
             .bind(id)
-            .map(|row: SqliteRow| Link{
-                id: row.get("id"),
-                url: row.get("url"),
-                shorturl: row.get("shorturl"),
-                title: row.get("title"),
-                description: row.get("description"),
-                private: row.get("private"),
-                created: row.get("created"),
-                updated: row.get("updated"),
-            })
+            .map(Self::from_row)
             .fetch_one(pool.get_ref())
             .await
+    }
+
+    pub async fn read_all(pool: &web::Data<SqlitePool>) -> Result<Vec<Link>, Error>{
+        let sql = "SELECT * FROM links;";
+        query(sql)
+            .map(Self::from_row)
+            .fetch_all(pool.get_ref())
+            .await
+    }
+
+    pub async fn delete(pool: &web::Data<SqlitePool>, link_id: i64) -> Result<Link, Error>{
+        let sql = "DELETE FROM links WHERE id = $1 RETURNING *;";
+        query(sql)
+            .bind(link_id)
+            .map(Self::from_row)
+            .fetch_one(pool.get_ref())
+            .await
+    }
+
+    pub async fn drop(pool: &web::Data<SqlitePool>) -> Result<SqliteQueryResult, Error>{
+        let sql = "DELETE FROM links;";
+        query(sql)
+            .execute(pool.get_ref())
+            .await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, path::Path};
+    use sqlx::{Sqlite, Pool, sqlite::SqlitePoolOptions, migrate::{Migrator,
+        MigrateDatabase}};
+    use actix_web::web::Data;
+    use super::{Tag, Link, LinkWithTagsNew};
+    use dotenv::dotenv;
+
+    async fn setup() -> Data<Pool<Sqlite>>{
+        dotenv().ok();
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+        if !Sqlite::database_exists(&db_url).await.unwrap(){
+            Sqlite::create_database(&db_url).await.unwrap()
+        }
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let migrations = Path::new(&crate_dir).join("migrations");
+        println!("{}", migrations.to_str().unwrap());
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(1)
+        .connect(&db_url)
+        .await
+        .expect("pool failed");
+
+    Migrator::new(migrations)
+        .await.unwrap()
+        .run(&pool)
+        .await.unwrap();
+
+        Data::new(pool)
+    }
+
+    async fn teardown(pool: &Data<Pool<Sqlite>>){
+        let _result = Link::drop(pool).await;
+    }
+
+    #[tokio::test]
+    async fn create(){
+        let pool = setup().await;
+        let new_link = LinkWithTagsNew {
+            url: "https://atareao.es".to_string(),
+            title: None,
+            description: None,
+            tags: None,
+            private: None,
+            created: None,
+            updated: None,
+        };
+
+        match Link::create_from_post(&pool, &new_link).await {
+            Ok(link) => {
+                assert_eq!(&link.url, &new_link.url);
+            },
+            Err(_) => assert!(false),
+        }
+        teardown(&pool).await;
+    }
+
+    #[tokio::test]
+    async fn read_one(){
+        let pool = setup().await;
+        match Link::create(&pool, "https://atareao.es").await {
+            Ok(link) => {
+                let test = Link::read(&pool, link.id).await.unwrap();
+                assert_eq!(test.id, link.id);
+            },
+            Err(_) => assert!(false),
+        }
+        teardown(&pool).await;
+    }
+
+    #[tokio::test]
+    async fn read_all(){
+        let pool = setup().await;
+        let _ = Link::create(&pool, "https://atareao.es").await;
+        let _ = Link::create(&pool, "https://google.es").await;
+        let _ = Link::create(&pool, "https://github.com").await;
+        let links = Link::read_all(&pool).await.unwrap();
+        assert_eq!(links.len(), 3);
+        teardown(&pool).await;
+    }
+
+    #[tokio::test]
+    async fn delete(){
+        let pool = setup().await;
+        let link = Link::create(&pool, "etiqueta 4").await.unwrap();
+        let _ = Link::delete(&pool, link.id).await;
+        let tags = Link::read_all(&pool).await.unwrap();
+        assert_eq!(tags.len(), 0);
+        teardown(&pool).await;
     }
 }
