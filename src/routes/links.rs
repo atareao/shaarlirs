2a994d88def9d0c88177bbcad309329a3c307d96
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-10-31 07:33:58 +0100

    create new routes

--- src/routes/links.rs
+++ src/routes/links.rs
@@ -1,21 +1,28 @@
 use actix_web::{get, post, put, delete, web, error::{ErrorNotFound,
     ErrorConflict, ErrorUnauthorized}, Error, HttpResponse};
+use sqlx::SqlitePool;
 
-use crate::models::{link::{LinkWithTags, Link}, metatag::Metatag};
+use crate::models::link::{LinkWithTagsNew, Link};
 
 
-/*
-pub async fn create(pool: &web::Data<PgPool>, new_link_with_tags: web::Json<LinkWithTags>) -> Result<HttpResponse, Error>{
-
-    let link = Link::create(&pool, url)
-            let name = category.into_inner().name;
-            Category::new(pool, &name, user_id)
-                .await
-                .map(|item| HttpResponse::Ok().json(item))
-                .map_err(|e| ErrorConflict(e))
-        },
-        Err(e) => Err(ErrorUnauthorized(e)),
-    }
+#[post("/links")]
+pub async fn create(pool: web::Data<SqlitePool>, new_link_with_tags: web::Json<LinkWithTagsNew>) -> Result<HttpResponse, Error>{
+    Link::create_from_post(&pool, &new_link_with_tags)
+        .await
+        .map(|item| HttpResponse::Ok().json(item))
+        .map_err(|e| ErrorConflict(e))
 }
 
-*/
+#[get("/links")]
+pub async fn read(pool: web::Data<SqlitePool>,
+        offset: web::Path<Option<i32>>,
+        limit: web::Path<Option<&str>>,
+        searchterm: web::Path<Option<&str>>,
+        searchtags: web::Path<Option<&str>>,
+        visibility: web::Path<Option<&str>>,
+) -> Result<HttpResponse, Error>{
+    Link::search(&pool, offset, limit, searchterm, searchtags, visibility)
+        .await
+        .map(|item| HttpResponse::Ok().json(item))
+        .map_err(|e| ErrorConflict(e))
+}
