a6934a60bf2482f22b6f57fe4ce072969ece391a
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-20 07:57:21 +0100

    complete links

--- src/routes/links.rs
+++ src/routes/links.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, post, put, delete, web, error::{ErrorNotFound,
     ErrorConflict, ErrorUnauthorized}, Error, HttpResponse};
-use sqlx::SqlitePool;
+use serde_json::json;
+use sqlx::{SqlitePool, error::Error::Database};
 use serde::Deserialize;
 use log::debug;
 
@@ -16,36 +17,47 @@ struct Params{
 
 
 #[post("/links")]
-pub async fn create(pool: web::Data<SqlitePool>, new_link_with_tags: web::Json<LinkWithTagsNew>) -> Result<HttpResponse, Error>{
-    Link::create_from_post(&pool, &new_link_with_tags)
-        .await
-        .map(|item| HttpResponse::Ok().json(item))
-        .map_err(|e| ErrorConflict(e))
+pub async fn create(pool: web::Data<SqlitePool>, new_link_with_tags: web::Json<LinkWithTagsNew>) -> HttpResponse{
+    match Link::create_from_post(&pool, &new_link_with_tags).await{
+        Ok(item) => HttpResponse::Created().json(item),
+        Err(e) => {
+            if let Some(err) = e.as_database_error(){
+                debug!("{:?}", err);
+                HttpResponse::Conflict().json(
+                    json!({"code": 409, "message": e.to_string()}))
+            }else{
+                HttpResponse::BadRequest().json(
+                    json!({"code": 400, "message": e.to_string()}))
+            }
+        },
+    }
 }
 
 #[get("/links")]
 pub async fn read(pool: web::Data<SqlitePool>, params: web::Query<Params>
-) -> Result<HttpResponse, Error>{
+) -> HttpResponse{
     debug!("Path: /links");
     let offset = &params.offset;
     let limit = &params.limit;
     let searchterm = &params.searchterm;
     let searchtags = &params.searchtags;
     let visibility = &params.visibility;
-    Link::search(&pool, offset, limit, searchterm, searchtags, visibility)
-        .await
-        .map(|item| HttpResponse::Ok().json(item))
-        .map_err(|e| ErrorConflict(e))
+    match Link::search(&pool, offset, limit, searchterm, searchtags, visibility)
+        .await{
+            Ok(items) => HttpResponse::Ok().json(items),
+            Err(_) => HttpResponse::BadRequest().json(
+                json!({"code": 400, "message": "Invalid parameters"})),
+        }
 }
 
 #[get("/links/{link_id}")]
 pub async fn read_one(pool: web::Data<SqlitePool>, link_id: web::Path<i64>,
-) -> Result<HttpResponse, Error>{
+) ->HttpResponse{
     debug!("Path: /links/{}", link_id);
-    Link::read(&pool, link_id.into_inner())
-        .await
-        .map(|item| HttpResponse::Ok().json(item))
-        .map_err(|e| ErrorConflict(e))
+    match Link::read(&pool, link_id.into_inner()).await{
+        Ok(item) => HttpResponse::Ok().json(item),
+        Err(_) => HttpResponse::BadRequest().finish(),
+    }
 }
 
 #[put("/links/{link_id}")]
